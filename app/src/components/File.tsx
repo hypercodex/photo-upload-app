598cab659f1092db0a2a8d3b17fbd32260992510
hypercodex <hypercodex@protonmail.com>
2020-10-11 00:05:11 -0700

    style file component with outset frame and cover

--- app/src/components/File.tsx
+++ app/src/components/File.tsx
@@ -10,6 +10,16 @@ import style from './File.module.scss'
 
 
 
+const Outset: React.FC<{orient: string}> = ({orient, children}) => 
+  <div className={style.outsetWrap}>
+    <div className={
+      orient === 'top' ? style.topOutset : style.bottomOutset
+      } >
+      {children}
+    <div></div>
+    </div>
+  </div>
+
 const FileUpload: React.FC<{idx: number; file: File}> = React.memo(({idx, file}) => {
   const { handleDelete, refetch } = useContext(GraphQLContext)
   const clickHandler = () => {
@@ -18,23 +28,23 @@ const FileUpload: React.FC<{idx: number; file: File}> = React.memo(({idx, file})
   } 
   return (
     <>
-        <div className={style.panel}>
+      <div className={style.panel}>
         <img src={file.url} className={style.image} />
           <div className={style.overlay}>
-            <div className={style.name}>
-             Doc {++idx}
-            {/* {file.name} */}
-            </div>
-            <div className={style.foot}>
-              <div className={style.size}>
-              {toKilobytes(file.size)}
-              </div>
-            <Button
-              clickHandler={clickHandler}
-              extraClass={style.button}
-            >
-              Delete
-            </Button>
+              <Outset orient={'top'}>
+                Doc {++idx}
+              </Outset>
+              <div className={style.underlay}></div>
+              <div className={style.foot}>
+              <Outset orient={'bottom'} > 
+                {toKilobytes(file.size)}
+              </Outset>
+              <Button
+                clickHandler={clickHandler}
+                extraClass={style.button}
+              >
+                Delete
+              </Button>
           </div>
         </div>
       </div>
